2d39b6f4be6a68de8e7f8494aeb1056270572fe3
Kenneth Rodriguez <cyborg.kenneth@gmail.com>
2023-09-16 01:22:36 -0600

    Finally fixed the Wave-Function Collapse algorithm

--- src/game.gd
+++ src/game.gd
@@ -5,129 +5,146 @@ var room_height: int
 
 var stack: Array[Vector2i] = []
 
+@onready var ui: CanvasLayer = get_node("UI")
+
 @onready var generate_button: Button = get_node("UI/HBoxContainer/GenerateButton")
 @onready var speed_button: Button = get_node("UI/HBoxContainer/SpeedButton")
+@onready var screenshot_button: Button = get_node("UI/HBoxContainer/ScreenshotButton")
+
+@onready var sea_slider: HSlider = get_node("UI/PanelContainer/Probabilities/VBoxContainer2/SeaSlider")
+@onready var coast_slider: HSlider = get_node("UI/PanelContainer/Probabilities/VBoxContainer2/CoastSlider")
+@onready var land_slider: HSlider = get_node("UI/PanelContainer/Probabilities/VBoxContainer2/LandSlider")
+@onready var mountain_slider: HSlider = get_node("UI/PanelContainer/Probabilities/VBoxContainer2/MountainSlider")
 
 enum {
-	WATER, 
-	SAND, 
-	GRASS, 
-	TREE
+	SEA, 
+	COAST, 
+	LAND, 
+	MOUNTAIN
 }
 
 enum {
-	INSTANT, 
+	INSTANT,
 	SLOW, 
 	MEDIUM, 
 	FAST
 }
 
 var distributions = {
-	WATER: {
-		WATER: 7, 
-		SAND: 3, 
-		GRASS: 0, 
-		TREE: 0
-	}, 
-	SAND: {
-		WATER: 3, 
-		SAND: 5, 
-		GRASS: 2,
-		TREE: 0
-	}, 
-	GRASS: {
-		WATER: 0, 
-		SAND: 2, 
-		GRASS: 6, 
-		TREE: 2
-	}, 
-	TREE: {
-		WATER: 0, 
-		SAND: 0, 
-		GRASS: 7, 
-		TREE: 3
-	}
+	SEA: [SEA, COAST], 
+	COAST: [SEA, COAST, LAND],
+	LAND: [COAST, LAND, MOUNTAIN], 
+	MOUNTAIN: [LAND, MOUNTAIN]
+}
+
+var probabilities = {
+	SEA: 1, 
+	COAST: 1, 
+	LAND: 1, 
+	MOUNTAIN: 1
 }
 
 signal world_filled
 
-var speed: int = 0: set = set_speed
-var timer_speed = 0
+var speed: int = SLOW: set = set_speed
+var timer_speed = 0.1
 
 func _ready():
 	room_width = ProjectSettings.get("display/window/size/viewport_width")/16
 	room_height = ProjectSettings.get("display/window/size/viewport_height")/16
-	fill_world()
+
+func get_valid_tiles(xx: int, yy: int)->Array: 
+	var tiles = [SEA, COAST, LAND, MOUNTAIN]
+	var atlas_coord: int = -1
+	var valid_tiles = []
+	for tile in tiles: 
+		if xx > 0: 
+			atlas_coord = get_cell_atlas_coords(0, Vector2i(xx - 1, yy)).x
+			if atlas_coord != -1: 
+				if tile not in distributions[atlas_coord]: 
+					continue
+		if xx < room_width - 1: 
+			atlas_coord = get_cell_atlas_coords(0, Vector2i(xx + 1, yy)).x
+			if atlas_coord != -1: 
+				if tile not in distributions[atlas_coord]: 
+					continue
+		if yy > 0: 
+			atlas_coord = get_cell_atlas_coords(0, Vector2i(xx, yy - 1)).x
+			if atlas_coord != -1: 
+				if tile not in distributions[atlas_coord]: 
+					continue
+		if yy < room_height - 1: 
+			atlas_coord = get_cell_atlas_coords(0, Vector2i(xx, yy + 1)).x
+			if atlas_coord != -1: 
+				if tile not in distributions[atlas_coord]: 
+					continue
+		valid_tiles.append(tile)
+	return valid_tiles
 	
-func get_empty_neighbors(xx: int, yy: int): 
+func get_lowest_entropy_tile(tiles_to_check: Array[Vector2i]): 
+	var lowest_entropy_tile = Vector2i(-1, -1)
+	var lowest_entropy = INF
+	
+	for tile in tiles_to_check: 
+		if get_cell_atlas_coords(0, tile).x != -1: 
+			continue
+		var valid_tiles = get_valid_tiles(tile.x, tile.y)
+		if valid_tiles.size() < lowest_entropy: 
+			lowest_entropy = valid_tiles.size()
+			lowest_entropy_tile = tile
+	return lowest_entropy_tile
+	
+func get_empty_neighbors(tile: Vector2i): 
 	var neighbors = []
-	if xx > 0 and get_cell_source_id(0, Vector2i(xx - 1, yy)) == -1: 
-		neighbors.append(Vector2i(xx - 1, yy))
-	if xx < room_width - 1 and get_cell_source_id(0, Vector2i(xx + 1, yy)) == -1: 
-		neighbors.append(Vector2i(xx + 1, yy) )
-	if yy > 0 and get_cell_source_id(0, Vector2i(xx, yy - 1)) == -1: 
-		neighbors.append(Vector2i(xx, yy - 1))
-	if yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx, yy + 1)) == -1: 
-		neighbors.append(Vector2i(xx, yy + 1))
-	if xx > 0 and yy > 0 and get_cell_source_id(0, Vector2i(xx - 1, yy - 1)) == -1: 
-		neighbors.append(Vector2i(xx - 1, yy - 1))
-	if xx > 0 and yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx - 1, yy + 1)) == -1: 
-		neighbors.append(Vector2i(xx - 1, yy + 1))
-	if xx < room_width - 1 and yy > 0 and get_cell_source_id(0, Vector2i(xx + 1, yy - 1)) == - 1: 
-		neighbors.append(Vector2i(xx + 1, yy - 1))
-	if xx < room_width - 1 and yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx + 1, yy + 1)) == -1: 
-		neighbors.append(Vector2i(xx + 1, yy + 1))
-		
+	var atlas_coord = -1
+	if tile.x > 0: 
+		atlas_coord = get_cell_atlas_coords(0, tile + Vector2i(-1, 0))
+		if atlas_coord.x == -1: 
+			neighbors.append(tile + Vector2i(-1, 0))
+	if tile.x < room_width - 1: 
+		atlas_coord = get_cell_atlas_coords(0, tile + Vector2i(1, 0))
+		if atlas_coord.x == -1: 
+			neighbors.append(tile + Vector2i(1, 0))
+	if tile.y > 0: 
+		atlas_coord = get_cell_atlas_coords(0, tile + Vector2i(0, -1))
+		if atlas_coord.y == -1: 
+			neighbors.append(tile + Vector2i(0, -1))
+	if tile.y < room_height - 1: 
+		atlas_coord = get_cell_atlas_coords(0, tile + Vector2i(0, 1))
+		if atlas_coord.y == -1: 
+			neighbors.append(tile + Vector2i(0, 1))
 	return neighbors
 	
-func get_full_neighbors(xx: int, yy: int): 
-	var neighbors = []
-	if xx > 0 and get_cell_source_id(0, Vector2i(xx - 1, yy)) != -1: 
-		neighbors.append(Vector2i(xx - 1, yy))
-	if xx < room_width - 1 and get_cell_source_id(0, Vector2i(xx + 1, yy)) != -1: 
-		neighbors.append(Vector2i(xx + 1, yy) )
-	if yy > 0 and get_cell_source_id(0, Vector2i(xx, yy - 1)) != -1: 
-		neighbors.append(Vector2i(xx, yy - 1))
-	if yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx, yy + 1)) != -1: 
-		neighbors.append(Vector2i(xx, yy + 1))
-	if xx > 0 and yy > 0 and get_cell_source_id(0, Vector2i(xx - 1, yy - 1)) != -1: 
-		neighbors.append(Vector2i(xx - 1, yy - 1))
-	if xx > 0 and yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx - 1, yy + 1)) != -1: 
-		neighbors.append(Vector2i(xx - 1, yy + 1))
-	if xx < room_width - 1 and yy > 0 and get_cell_source_id(0, Vector2i(xx + 1, yy - 1)) != - 1: 
-		neighbors.append(Vector2i(xx + 1, yy - 1))
-	if xx < room_width - 1 and yy < room_height - 1 and get_cell_source_id(0, Vector2i(xx + 1, yy + 1)) != -1: 
-		neighbors.append(Vector2i(xx + 1, yy + 1))
-	return neighbors
-	
-func valid_area(cell: Vector2i): 
-	var options = []
-	var neighbors = get_full_neighbors(cell.x, cell.y)
-	for n in neighbors: 
-		var tile = get_cell_atlas_coords(0, n).x
-		for key in distributions[tile]:
-			for i in range(distributions[tile][key]): 
-				options.append(key)
-	return options
+func get_probability_distribution(tiles: Array): 
+	var probability_distribution = []
+	for tile in tiles: 
+		for i in range(probabilities[tile]): 
+			probability_distribution.append(tile)
+			
+	return probability_distribution
 
 func fill_world(): 
-	var xx = int(room_width / 2)
-	var yy = int(room_height / 2)
-	set_cell(0, Vector2i(xx, yy), 0, Vector2i(randi_range(WATER, TREE), 0))
-	for n in get_empty_neighbors(xx, yy): 
-		stack.append(n)
-	while  stack.size() > 0: 
-		var front: Vector2i = stack.pop_front()
-		if get_cell_source_id(0, front) != -1: 
-			continue
-		var options = valid_area(front)
-		if options.size() > 0: 
-			set_cell(0, front, 0, Vector2i(options.pick_random(), 0))
-			for n in get_empty_neighbors(front.x, front.y): 
-				if n not in stack: 
-					stack.append(n)
-			if speed != INSTANT: 
-				await get_tree().create_timer(timer_speed).timeout
+	
+	var tiles_to_check: Array[Vector2i] = []
+	var xx = randi() % room_width
+	var yy = randi() % room_height
+	
+	while xx != -1 and yy != -1: 
+		var valid_tiles = get_valid_tiles(xx, yy)
+		
+		set_cell(0, Vector2i(xx, yy), 0, Vector2i(get_probability_distribution(valid_tiles).pick_random(), 0))
+		
+		var neighbors = get_empty_neighbors(Vector2i(xx, yy))
+		tiles_to_check.append_array(neighbors)
+		
+		var next_tile = get_lowest_entropy_tile(tiles_to_check)
+		tiles_to_check.erase(next_tile)
+		
+		xx = next_tile.x
+		yy = next_tile.y
+		if speed != INSTANT: 
+			await  get_tree().create_timer(timer_speed).timeout
+
 	emit_signal("world_filled")
 		
 func clean(): 
@@ -153,15 +170,51 @@ func set_speed(value):
 
 func _on_world_filled():
 	generate_button.disabled = false
+	screenshot_button.disabled = false
 
 func _on_speed_button_pressed():
 	self.speed += 1
 
 func _on_generate_button_pressed():
 	generate_button.disabled = true
+	screenshot_button.disabled = true
 	clean()
 	fill_world()
 
 
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://Menus/start_menu.tscn")
+
+
+func _on_sea_slider_drag_ended(value_changed):
+	if value_changed: 
+		probabilities[SEA] = wrapi(sea_slider.value, 1, 11)
+
+
+func _on_coast_slider_drag_ended(value_changed):
+	if value_changed: 
+		probabilities[COAST] = wrapi(coast_slider.value, 1, 11)
+
+
+func _on_land_slider_drag_ended(value_changed):
+	if value_changed: 
+		probabilities[LAND] = wrapi(land_slider.value, 1, 11)
+		
+func _on_mountain_slider_drag_ended(value_changed):
+	if value_changed: 
+		probabilities[MOUNTAIN] = wrapi(mountain_slider.value, 1, 11)
+
+
+func _on_screenshot_button_pressed():
+	
+	ui.hide()
+	
+	await get_tree().create_timer(1).timeout
+	
+	var viewport: Viewport = get_viewport()
+	var result = viewport.get_texture().get_image().save_png("user://screenshot_"+str(Time.get_unix_time_from_system())+".png")
+	viewport.get_texture().get_image()
+	await get_tree().create_timer(1).timeout
+	
+	ui.show()
+	#viewport.get_texture().get_image().save_png("user://")
